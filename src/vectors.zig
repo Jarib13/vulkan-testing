82fb34278acdf749e9ee4ca6130c73af76e0c208
jarib <me@jared.ooo>
2023-10-26 18:11:36 -0700

    vecs

--- src/vectors.zig
+++ src/vectors.zig
@@ -210,3 +210,111 @@ pub const vec3 = extern struct {
         return self.add(dir.mul(dis));
     }
 };
+
+//Column Major Ordering
+const mat4 = struct {
+    values: @Vector(4 * 4, f32),
+
+    fn zeroes() mat4 {
+        return mat4{
+            .values = @splat(0),
+        };
+    }
+
+    fn identity() mat4 {
+        return mat4{
+            .values = .{ 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
+        };
+    }
+
+    fn scale(x: f32, y: f32, z: f32) mat4 {
+        return mat4{
+            .values = .{ x, 0, 0, 0, 0, y, 0, 0, 0, 0, z, 0, 0, 0, 0, 1 },
+        };
+    }
+
+    fn translation(x: f32, y: f32, z: f32) mat4 {
+        return mat4{
+            .values = .{ 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, x, y, z, 1 },
+        };
+    }
+
+    fn rotationY(angle: f32) mat4 {
+        return mat4{
+            .values = .{ @cos(angle), 0, @sin(angle), 0, 0, 1, 0, 0, -@sin(angle), 0, @cos(angle), 0, 0, 0, 0, 1 },
+        };
+    }
+
+    fn rotationX(angle: f32) mat4 {
+        return mat4{
+            .values = .{ 1, 0, 0, 0, 0, @cos(angle), -@sin(angle), 0, 0, @sin(angle), @cos(angle), 0, 0, 0, 0, 1 },
+        };
+    }
+
+    fn rotationZ(angle: f32) mat4 {
+        return mat4{
+            .values = .{ @cos(angle), @sin(angle), 0, 0, -@sin(angle), @cos(angle), 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
+        };
+    }
+
+    fn perspective(fov: f32, aspect_ratio: f32, near: f32, far: f32) mat4 {
+        var t = @tan(fov / 2);
+        return mat4{
+            .values = .{ 1 / (t * aspect_ratio), 0, 0, 0, 0, 1 / t, 0, 0, 0, 0, -(far + near) / (far - near), -1, 0, 0, -(2 * far * near) / (far - near), 1 },
+        };
+    }
+
+    fn multiply(self: mat4, other: mat4) mat4 {
+        var result: mat4 = mat4.zeroes();
+
+        for (0..4) |res_col| {
+            for (0..4) |res_row| {
+                var value: f32 = 0;
+                for (0..4) |self_col_other_row| {
+                    value += self.values[self_col_other_row * 4 + res_row] * other.values[self_col_other_row + res_col * 4];
+                }
+                result.values[res_col * 4 + res_row] = value;
+            }
+        }
+
+        return result;
+    }
+
+    fn approx_equals(self: mat4, other: mat4) bool {
+        return equals(self, other, 0.001);
+    }
+
+    fn equals(self: mat4, other: mat4, tolerance: f32) bool {
+        for (0..16) |i| {
+            if (!std.math.approxEqAbs(f32, self.values[i], other.values[i], tolerance))
+                return false;
+        }
+
+        return true;
+    }
+
+    fn print(self: mat4) void {
+        std.debug.print("\n", .{});
+        for (0..4) |row| {
+            std.debug.print("[ ", .{});
+            for (0..4) |col| {
+                std.debug.print("{d:.3}, ", .{self.values[col * 4 + row]});
+            }
+            std.debug.print("]\n", .{});
+        }
+    }
+};
+
+test "matrix" {
+    const assert = std.debug.assert;
+
+    var scale = mat4.scale(2, 2, 2);
+    var translate = mat4.translation(0, 5, 0);
+    var identity = mat4.identity();
+    identity.multiply(translate).print();
+    identity.multiply(identity.multiply(translate)).print();
+    identity.multiply(scale.multiply(translate)).print();
+    identity.multiply(scale.multiply(scale)).print();
+    assert(identity.multiply(translate).approx_equals(translate));
+    assert(identity.multiply(translate).approx_equals(identity.multiply(translate)));
+}
